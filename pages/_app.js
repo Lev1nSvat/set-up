1091fcb7c630d7df7c0299450b965613de0db6ec
Lev1nSvat <89538630+Lev1nSvat@users.noreply.github.com>
2022-09-21 16:03:23 +0300

    Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import React, { useState, useEffect } from 'react';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    let scroll;
+    import("locomotive-scroll").then((locomotiveModule) => {
+        scroll = new locomotiveModule.default({
+            el: document.querySelector("[data-scroll-container]"),
+            smooth: true,
+            smoothMobile: false,
+            resetNativeScroll: true
+        });
+    });
+
+    // `useEffect`'s cleanup phase
+    return () => {
+        if (scroll) scroll.destroy();
+    }
+});
+  return (
+    <div data-scroll-container>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
